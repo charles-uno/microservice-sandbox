42710588f3836b896548df6252bc316f066cea13
Charles McEachern <charles.a.mceachern@gmail.com>
2019-01-20 09:40:09 -0600

    got it running!

--- go-net-http/main.go
+++ go-net-http/main.go
@@ -1,21 +1,59 @@
 package main
 
 import (
+    "encoding/json"
     "fmt"
     "log"
     "net/http"
+    "strconv"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Welcome to the HomePage!")
-    fmt.Println("Endpoint Hit: homePage")
+// =====================================================================
+
+func get_factors(n int) []int {
+    factors := []int{}
+    for p := 2; n > 1 ; p++ {
+        // If n % p == 0, p must be prime. We have already checked
+        // against everything smaller.
+        for n % p == 0 {
+            n = n/p
+            factors = append(factors, p)
+        }
+    }
+    return factors
 }
 
-func handleRequests() {
-    http.HandleFunc("/", homePage)
-    log.Fatal(http.ListenAndServe(":8081", nil))
+// =====================================================================
+
+func factors(w http.ResponseWriter, r *http.Request) {
+    // Report hits in the terminal.
+    fmt.Printf("Hit URL: %s%s\n", r.Host, r.URL.Path)
+    // Grab the given number. Third-party tools do this better.
+    n, err := strconv.Atoi( r.URL.Path[9:] )
+    if err == nil && n > 0 {
+        out, _ := json.MarshalIndent(get_factors(n), "", "    ")
+        fmt.Fprintf(w, string(out) + "\n")
+    } else {
+        w.WriteHeader(400)
+        fmt.Fprintln(w, "{\"error\": \"Bad input\"}")
+    }
 }
+
+// =====================================================================
+
+func catchall(w http.ResponseWriter, r *http.Request) {
+    // Report hits in the terminal.
+    fmt.Printf("Hit URL: %s%s\n", r.Host, r.URL.Path)
+    w.WriteHeader(404)
+    fmt.Fprintln(w, "{\"error\": \"You're lost\"}")
+}
+
+// =====================================================================
 
 func main() {
-    handleRequests()
+    handler := http.NewServeMux()
+    handler.HandleFunc("/factors/", factors)
+    handler.HandleFunc("/", catchall)
+    err := http.ListenAndServe(":8081", handler)
+    log.Fatal(err)
 }
